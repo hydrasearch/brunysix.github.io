16078542d30b43d835937724e65817c3a4eec507
hydrasearch <150490752+hydrasearch@users.noreply.github.com>
2025-01-28 17:53:13 -0600

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://skibzy.global.ssl.fastly.net/bare/',
+    bare:'https://bare-sigma.yourmom.us.kg/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
